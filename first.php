0464e965f1df4c10c644015fcac858bc7479961c
DipkonaRoy <dipkona.roy@gmail.com>
2021-01-30 15:54:13 +0530

    Create first.php

---
+++ first.php
@@ -0,0 +1,13 @@
+ <?php
+ echo "<h1>Welcome to my Github Account</h1>";
+  $a = 10;
+  $b = 10;
+  $c = $a + $b;
+  echo $c;
+  function test(){
+     global $a,$c;
+     $ = $c-$a;
+  }
+  test();
+  echo $d;
+  ?>
